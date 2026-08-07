447daa148f610dcb2fab1e557c74fcba99ec9523
Himanshu Baunthiyal <himanshu.baunthiyal07@gmail.com>
2026-08-07 11:00:24 +0530

    remove app.options() from app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,9 +9,6 @@ app.use(cors({
     credentials: true,
 }))
 
-app.options('*', cors())
-
-console.log("CORS_ORIGIN is:", process.env.CORS_ORIGIN)
 
 // Webhook route needs RAW body — must come BEFORE express.json()
 app.use('/api/v1/payments/webhook', express.raw({ type: 'application/json' }))
