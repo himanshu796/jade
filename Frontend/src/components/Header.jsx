3daac5fd48af9de83bd99edbfc747c3dcbacd342
Himanshu Baunthiyal <himanshu.baunthiyal07@gmail.com>
2026-08-11 12:04:06 +0530

    space between logo and resort name corrected

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -80,8 +80,8 @@ const Header = () => {
         </div>
 
         {/* Center - Logo & Hotel Name  */}
-        <div className="flex items-center justify-center gap-2 flex-1 mx-2 min-w-0">
-          <Link to="/" className="flex items-center gap-1 shrink-0">
+        <div className="flex items-center justify-center gap-1 flex-1 mx-2 min-w-0">
+          <Link to="/" className="flex items-center shrink-0">
             <img
               src={jadeLogoImg}
               alt="Jade logo"
